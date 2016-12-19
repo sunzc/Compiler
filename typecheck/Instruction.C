910345f75dd64c4810845257818675fcb7805b16
Zhichuang Sun <zhisun@cs.stonybrook.edu>
2016-12-19 17:23:20 -0500

    add toString implementation for Instruction and Operand
    Signed-off-by: Zhichuang Sun <zhisun@cs.stonybrook.edu>

--- typecheck/Instruction.C
+++ typecheck/Instruction.C
@@ -8,6 +8,47 @@ ArithIns::ArithIns(ArithInsType at, const Operand* arg1, const Operand* arg2, co
 		arg2_ = arg2;
 		dest_ = result;
 }
+
+std::string ArithIns::toString(){
+	string inst = NULL;
+
+	switch(at_) {
+		case ArithInsType::ADD:
+			inst = "ADD ";
+			break;
+		case ArithInsType::SUB:
+			inst = "SUB ";
+			break;
+		case ArithInsType::DIV:
+			inst = "DIV ";
+			break;
+		case ArithInsType::MUL:
+			inst = "MUL ";
+			break;
+		case ArithInsType::MOD:
+			inst = "MOD ";
+			break;
+		case ArithInsType::NEG:
+			inst = "NEG ";
+			break;
+		case ArithInsType::AND:
+			inst = "AND ";
+			break;
+		case ArithInsType::OR:
+			inst = "OR ";
+			break;
+		case ArithInsType::XOR:
+			inst = "XOR ";
+			break;
+		default:
+			std::cout<<"Unknown ArithInsType!"<<std::endl;
+			break;
+	}
+
+ 	inst += arg1_->toString() + " " + arg2_->toString() + " " + dest_->toString()+"\n";
+
+	return inst;
+}
             
 FloatArithIns::FloatArithIns(FloatArithInsType fat, const Operand* arg1, const Operand* arg2, const Operand* result)
         :Instruction(Instruction::InstructionType::FLOAT){
@@ -16,12 +57,73 @@ FloatArithIns::FloatArithIns(FloatArithInsType fat, const Operand* arg1, const O
 		arg2_ = arg2;
 		dest_ = result;
 }
+
+std::string FloatArithIns::toString(){
+	string inst = NULL;
+
+	switch(fat_) {
+		case FloatArithInsType::FADD:
+			inst = "FADD ";
+			break;
+		case FloatArithInsType::FSUB:
+			inst = "FSUB ";
+			break;
+		case FloatArithInsType::FDIV:
+			inst = "FDIV ";
+			break;
+		case FloatArithInsType::FMUL:
+			inst = "FMUL ";
+			break;
+		case FloatArithInsType::FNEG:
+			inst = "FNEG ";
+			break;
+		default:
+			std::cout<<"Unknown FloatArithInsType!"<<std::endl;
+			break;
+	}
+
+	inst += arg1_->toString() + " " + arg2_->toString() + " " + dest_->toString() + "\n";
+
+	return inst;
+}
             
 RelOpIns::RelOpIns(RelOpInsType relop, const Operand* arg1, const Operand* arg2)
         :Instruction(Instruction::InstructionType::RELOP){
 		relop_ = relop;
 		arg1_ = arg1;
 		arg2_ = arg2;
+}
+
+std::string RelOpIns::toString(){
+	string inst = NULL;
+
+	switch(relop_) {
+		case RelOpInsType::GT:
+			inst = "GT ";
+			break;
+		case RelOpInsType::GE:
+			inst = "GE ";
+			break;
+		case RelOpInsType::UGT:
+			inst = "UGT ";
+			break;
+		case RelOpInsType::UGE:
+			inst = "UGE ";
+			break;
+		case RelOpInsType::EQ:
+			inst = "EQ ";
+			break;
+		case RelOpInsType::NE:
+			inst = "NE ";
+			break;
+		default:
+			std::cout<<"Unknown RelOpIns!"<<std::endl;
+			break;
+	}
+
+	inst += arg1_->toString() + " " + arg2_->toString() + "\n";
+
+	return inst;
 }
             
 FloatRelOpIns::FloatRelOpIns(FloatRelOpInsType frelop, const Operand* arg1, const Operand* arg2)
@@ -30,24 +132,143 @@ FloatRelOpIns::FloatRelOpIns(FloatRelOpInsType frelop, const Operand* arg1, cons
 		arg1_ = arg1;
 		arg2_ = arg2;
 }
-            
+
+std::string FloatRelOpIns::toString(){
+	string inst = NULL;
+
+	switch(frelop_) {
+		case FloatRelOpInsType::FGT:
+			inst = "FGT ";
+			break;
+		case FloatRelOpInsType::FGE:
+			inst = "FGE ";
+			break;
+		case FloatRelOpInsType::FEQ:
+			inst = "FEQ ";
+			break;
+		case FloatRelOpInsType::FNE:
+			inst = "FNE ";
+			break;
+		default:
+			std::cout<<"Unknown RelOpInsType!"<<std::endl;
+			break;
+	}
+
+	inst += arg1_->toString() + " " + arg2_->toString() + "\n";
+
+	return inst;
+}
+
 PrintIns::PrintIns(PrintInsType pit, const Operand* arg1)
         :Instruction(Instruction::InstructionType::PRINT){
 		pit_ = pit;
 		arg1_ = arg1;
 }
-            
+
+std::string PrintIns::toString(){
+	string inst = NULL;
+
+	switch(pit_) {
+		case PrintInsType::PRTI:
+			inst = "PRTI ";
+			break;
+		case PrintInsType::PRTS:
+			inst = "PRTS ";
+			break;
+		case PrintInsType::PRTF:
+			inst = "PRTF ";
+			break;
+		default:
+			std::cout<<"Unknown PrintInsType!"<<std::endl;
+			break;
+	}
+
+	inst += arg1_->toString() + "\n";
+
+	return inst;
+}
+
 MovIns::MovIns(MovInsType mit, const Operand* arg1, const Operand* arg2)
         :Instruction(Instruction::InstructionType::DATAMOV){
 		mit_ = mit;
 		arg1_ = arg1;
 		arg2_ = arg2;
 }
-            
+
+std::string MovIns::toString(){
+	string inst = NULL;
+
+	switch(mit_) {
+		case MovInsType::MOVL:
+			inst = "MOVL ";
+			break;
+		case MovInsType::MOVS:
+			inst = "MOVS ";
+			break;
+		case MovInsType::MOVI:
+			inst = "MOVI ";
+			break;
+		case MovInsType::MOVF:
+			inst = "MOVF ";
+			break;
+		case MovInsType::MOVIF:
+			inst = "MOVIF ";
+			break;
+		case MovInsType::LDI:
+			inst = "LDI ";
+			break;
+		case MovInsType::LDF:
+			inst = "LDF ";
+			break;
+		case MovInsType::STI:
+			inst = "STI ";
+			break;
+		case MovInsType::STF:
+			inst = "STF ";
+			break;
+		default:
+			std::cout<<"Unknown PrintInsType!"<<std::endl;
+			break;
+	}
+
+	inst += arg1_->toString() + " " + arg2_->toString() + "\n";
+
+	return inst;
+}
+
 JumpIns::JumpIns(JumpInsType jit, Instruction *cond, Operand *arg1)
 	:Instruction(Instruction::InstructionType::JMP){
 		jit_ = jit;
 		cond_ = cond;
 		arg1_ = arg1;
+}
+
+std::string JumpIns::toString(){
+	string inst = NULL;
+	string cond = NULL;
+
+	switch(jit_) {
+		case JumpInsType::JMP:
+			inst = "JMP " + arg1_->toString() + "\n";
+			break;
+		case JumpInsType::JMPI:
+			inst = "JMPI " + arg1_->toString() + "\n";
+			break;
+		case JumpInsType::JMPC:
+			cond = cond_->toString();
+			cond.erase(cond.find_last_not_of("\n")+1);
+			inst = "JMPC " + cond + " " + arg1_->toString() + "\n";
+			break;
+		case JumpInsType::JMPCI:
+			cond = cond_->toString();
+			cond.erase(cond.find_last_not_of("\n")+1);
+			inst = "JMPCI " + cond + " " + arg1_->toString() + "\n";
+			break;
+		default:
+			std::cout<<"Unknown PrintInsType!"<<std::endl;
+			break;
+	}
+
+	return inst;
 }
 /* TODO: OTHER methods to be implemented */
--- typecheck/Instruction.h
+++ typecheck/Instruction.h
@@ -4,6 +4,7 @@
 /********************* class definition of an instruction and what it contains ****************************/
 
 #include <iostream>
+#include <string>
 
 class Instruction {
   public:
@@ -22,6 +23,7 @@ class Instruction {
 	/* An address can be a name, constant or a compiler-generated temporary */
 	
 	static std::string getLabel();
+	virtual std::string toString() { return NULL;};
 	
 	/* Operand class to be defined here */
 	
@@ -39,9 +41,49 @@ class Instruction {
 	        Operand(OperandType ot, float f_val) { ot_ = ot; f_val_ = f_val;};
 	        Operand(OperandType ot, std::string s_val) { ot_ = ot; s_val_ = s_val;};
 
-		int i_val() { return i_val_;}
-		float f_val() { return f_val_;}
-		std::string s_val() { return s_val_;}
+		int i_val() const { return i_val_;};
+		float f_val() const { return f_val_;};
+		std::string s_val() const { return s_val_;};
+		std::string reg_name(int i) const{
+			std::string name = NULL;
+
+			if (i>=0 && i<=9)
+				name = "00" + std::to_string(i);
+			else if (i>9 && i<=99)
+				name = "0" + std::to_string(i);
+			else if (i>99 && i<=999)
+				name = std::to_string(i);
+			else
+				std::cout<<"Illeagal register number:"<<i<<std::endl;
+
+			return name;
+		};
+
+		// return a corresponding operand name like 'R001','F003',"L2:","test output"
+		std::string toString() const{
+			std::string op = NULL;
+			switch (ot_) {
+				case OperandType::INT_REG:
+					op = "R" + reg_name(i_val_);
+					break;
+				case OperandType::FLOAT_REG:
+					op = "F" + reg_name(i_val_);
+					break;
+				case OperandType::INT_CONST:
+					op = std::to_string(i_val_);
+					break;
+				case OperandType::FLOAT_CONST:
+					op = std::to_string(f_val_);
+					break;
+				case OperandType::STR_CONST:
+					op = s_val();
+					break;
+				default:
+					std::cout<<"Unsupport Operand Type:"<<(int)ot_<<std::endl;
+			}
+
+			return op;
+		};
         
         	~Operand();
 	  private:
@@ -64,6 +106,7 @@ class ArithIns: public Instruction{
 			};
 	public:
 		ArithIns(ArithInsType at, const Operand* arg1, const Operand* arg2, const Operand* result);
+		std::string toString();
 	private:
         ArithInsType at_;
 	const Operand* arg1_;
@@ -78,6 +121,7 @@ class FloatArithIns: public Instruction{
 		};
 	public: /* TODO constructor */
 		FloatArithIns(FloatArithInsType fat, const Operand* arg1, const Operand* arg2, const Operand* result);
+		std::string toString();
 	private:
         FloatArithInsType fat_;
 	const Operand* arg1_;
@@ -93,6 +137,7 @@ class RelOpIns: public Instruction{
 		};
 	public: /* TODO constructor */
 		RelOpIns(RelOpInsType relop, const Operand* arg1, const Operand* arg2); /* No destination register */
+		std::string toString();
 	private:
         RelOpInsType relop_;
         const Operand* arg1_;
@@ -105,7 +150,8 @@ class FloatRelOpIns: public Instruction{
 			FGT, FGE, FEQ, FNE
 		};
 	public: /*TODO constructor */
-	    FloatRelOpIns(FloatRelOpInsType frelop, const Operand* arg1, const Operand* arg2);
+		FloatRelOpIns(FloatRelOpInsType frelop, const Operand* arg1, const Operand* arg2);
+		std::string toString();
 	private:
         FloatRelOpInsType frelop_;
         const Operand* arg1_;
@@ -120,6 +166,7 @@ class PrintIns: public Instruction{
 		};
 	public: /* TODO constructor */
 		PrintIns(PrintInsType pit, const Operand* arg1); /* Only one operand */
+		std::string toString();
 	private:
         PrintInsType pit_;
         const Operand* arg1_;
@@ -132,6 +179,7 @@ class JumpIns: public Instruction{
 		};
 	public: /* TODO constructor ---> how many operators */
 		JumpIns(JumpInsType jit, Instruction *cond, Operand* arg1);
+		std::string toString();
 	private:
         JumpInsType jit_;
 	Instruction *cond_; /* cond is a RelOpIns or FloatRelOpIns, required by JMPC/JMPCI */
@@ -147,7 +195,7 @@ class MovIns: public Instruction{
 		};
 	public: /* TODO constructor */
 		MovIns(MovInsType mit, const Operand* arg1, const Operand* arg2);  /* mov instructions have 2 operands */
-		~MovIns();
+		std::string toString();
 	private:
         MovInsType mit_;
         const Operand* arg1_;
